marten-seemann/nft

commit ceab53cee4999debd64ab29414b918746209ba7b

evaluate: don't allow merging interval set/map with non-interval one
Included bogon asserts with:
BUG: invalid data expression type range_value

Pablo says: "Reject because flags interval is lacking".
Make it so.

Signed-off-by: Florian Westphal <fw@strlen.de>
Reviewed-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/evaluate.c
@@ -5080,15 +5080,19 @@ static int set_evaluate(struct eval_ctx *ctx, struct set *set)
 			return table_not_found(ctx);
 
 		existing_set = set_cache_find(table, set->handle.set.name);
-		if (!existing_set)
+		if (existing_set) {
+			if (existing_set->flags & NFT_SET_EVAL) {
+				uint32_t existing_flags = existing_set->flags & ~NFT_SET_EVAL;
+				uint32_t new_flags = set->flags & ~NFT_SET_EVAL;
+
+				if (existing_flags == new_flags)
+					set->flags |= NFT_SET_EVAL;
+			}
+
+			if (set_is_interval(set->flags) && !set_is_interval(existing_set->flags))
+				return set_error(ctx, set, "existing %s lacks interval flag", type);
+		} else {
 			set_cache_add(set_get(set), table);
-
-		if (existing_set && existing_set->flags & NFT_SET_EVAL) {
-			uint32_t existing_flags = existing_set->flags & ~NFT_SET_EVAL;
-			uint32_t new_flags = set->flags & ~NFT_SET_EVAL;
-
-			if (existing_flags == new_flags)
-				set->flags |= NFT_SET_EVAL;
 		}
 	}
 

tests/shell/testcases/bogons/nft-f/invalid_data_expr_type_range_value_assert
@@ -1,0 +1,12 @@
+table ip x {
+	map y {
+		type ipv4_addr : ipv4_addr
+		elements = { 1.168.0.4 }
+	}
+
+        map y {
+		type ipv4_addr : ipv4_addr
+		flags interval
+		elements = { 10.141.3.0/24 : 192.8.0.3 }
+	}
+}

commit 3e50cd6b063d64c2e72b0e32bc36dd5a22f75c06

evaluate: move interval flag compat check after set key evaluation
Without this, included bogon asserts with:
BUG: unhandled key type 13
nft: src/intervals.c:73: setelem_expr_to_range: Assertion `0' failed.

... because we no longer evaluate set->key/data.

Move the check to the tail of the function, right before assiging
set->existing_set, so that set->key has been evaluated.

Fixes: ceab53cee499 ("evaluate: don't allow merging interval set/map with non-interval one")
Signed-off-by: Florian Westphal <fw@strlen.de>
Reviewed-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/evaluate.c
@@ -5088,9 +5088,6 @@ static int set_evaluate(struct eval_ctx *ctx, struct set *set)
 				if (existing_flags == new_flags)
 					set->flags |= NFT_SET_EVAL;
 			}
-
-			if (set_is_interval(set->flags) && !set_is_interval(existing_set->flags))
-				return set_error(ctx, set, "existing %s lacks interval flag", type);
 		} else {
 			set_cache_add(set_get(set), table);
 		}
@@ -5180,6 +5177,9 @@ static int set_evaluate(struct eval_ctx *ctx, struct set *set)
 
 		return 0;
 	}
+
+	if (existing_set && set_is_interval(set->flags) && !set_is_interval(existing_set->flags))
+		return set_error(ctx, set, "existing %s lacks interval flag", type);
 
 	set->existing_set = existing_set;
 

tests/shell/testcases/bogons/nft-f/invalid_data_expr_type_range_value_2_assert
@@ -1,0 +1,13 @@
+table inet t {
+        map m2 {
+                typeof udp length . @ih,32,32 : verdict
+                elements = {
+                             1-10 . 0xa : drop }
+        }
+
+	map m2 {
+                typeof udp length . @ih,32,32 : verdict
+                flags interval
+                elements = { 20-80 . 0x14 : accept }
+        }
+}