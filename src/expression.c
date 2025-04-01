marten-seemann/nft

commit 53d6bb9924455644f61d61b18e4d6cf47fc46684

expression: initialize list of expression to silence gcc compile warning
The helper function to translate flagcmp expression to binop expression
results in the following compile warning.

  src/expression.c: In function 'list_expr_to_binop':
  src/expression.c:1286:16: warning: 'last' may be used uninitialized [-Wmaybe-uninitialized]
  1286 |         return last;

While at it, add assert() to validate the premises where this function
can be called.

Fixes: 4d5990c92c83 ("src: transform flag match expression to binop expression from parser")
Reported-by: Florian Westphal <fw@strlen.de>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/expression.c
@@ -1266,7 +1266,9 @@ struct expr *list_expr_alloc(const struct location *loc)
 /* list is assumed to have two items at least, otherwise extend this! */
 struct expr *list_expr_to_binop(struct expr *expr)
 {
-	struct expr *first, *last, *i;
+	struct expr *first, *last = NULL, *i;
+
+	assert(!list_empty(&expr->expressions));
 
 	first = list_first_entry(&expr->expressions, struct expr, list);
 	i = first;
@@ -1279,6 +1281,9 @@ struct expr *list_expr_to_binop(struct expr *expr)
 			last = binop_expr_alloc(&expr->location, OP_OR, i, last);
 		}
 	}
+	/* list with one single item only, this should not happen. */
+	assert(first);
+
 	/* zap list expressions, they have been moved to binop expression. */
 	init_list_head(&expr->expressions);
 	expr_free(expr);

commit eebf7dd6582a2e94990388ba2d0772d14289b674

expression: incorrect assert() list_expr_to_binop
assert() logic is reversed, all expressions in the list are handled,
including the first.

  src/expression.c:1285: list_expr_to_binop: Assertion `first' failed.

Fixes: 53d6bb992445 ("expression: initialize list of expression to silence gcc compile warning")
Reported-by: Florian Westphal <fw@strlen.de>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
Signed-off-by: Florian Westphal <fw@strlen.de>

src/expression.c
@@ -1282,7 +1282,7 @@ struct expr *list_expr_to_binop(struct expr *expr)
 		}
 	}
 	/* list with one single item only, this should not happen. */
-	assert(first);
+	assert(!first);
 
 	/* zap list expressions, they have been moved to binop expression. */
 	init_list_head(&expr->expressions);